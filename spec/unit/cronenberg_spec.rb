a38dd95579fb7399a5bd431895bc08570efcca25
Zee Alexander <zee@zee.space>
2016-01-14 20:41:17 -0800

    Remove dummy test.

--- spec/unit/cronenberg_spec.rb
+++ spec/unit/cronenberg_spec.rb
@@ -4,8 +4,4 @@ describe Cronenberg do
   it 'has a version number' do
     expect(Cronenberg::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
