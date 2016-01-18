1fc1ed090ffbd273862884f7f6aa7d5e14eeea16
Zee Alexander <zee@zee.space>
2016-01-18 12:57:20 -0800

    Test that config initialize class can return items from envvars

---
+++ spec/unit/cronenberg__config_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'cronenberg/config'
+require 'rspec/mocks/standalone'
+
+describe Cronenberg::Config do
+  context "#initialize" do
+    let(:config_hash) do
+      {
+        host: 'vsphere.pizza.com',
+        user: 'pizzamaster5000',
+        password: 'pizzapass',
+      }
+    end
+
+    context "with no env variables or config file" do
+      it "should raise a RunTime error" do
+        expect{ Cronenberg::Config.new }.to raise_error(RuntimeError)
+      end
+    end
+
+    context "with a valid configuration hash" do
+      before do
+        allow_any_instance_of(Cronenberg::Config).to receive(:process_environment_variables).and_return(config_hash)
+      end
+      it "returns valid config items" do
+        config = Cronenberg::Config.new
+        expect(config.host).to eq(config_hash[:host])
+        expect(config.user).to eq(config_hash[:user])
+        expect(config.password).to eq(config_hash[:password])
+      end
+    end
+  end
+end
+
